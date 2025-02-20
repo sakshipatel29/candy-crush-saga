2adb3d38f0d2fb590c0a5e3df128cd4a4babe78b
sakshipatel29 <sakshirpatel29062002@gmail.com>
2025-02-20 16:55:56 -0600

    when we crush candies, rest will adjust place sliding down

--- candy.js
+++ candy.js
@@ -14,6 +14,7 @@ window.onload = function(){
 
     window.setInterval(function(){
         crushCandy();
+        slideCandy();
     }, 100);
 }
 
@@ -158,4 +159,19 @@ function checkValid(){
         }
     }
     return false;
+}
+
+function slideCandy(){
+    for(let c = 0; c < columns; c++){
+        let ind = rows-1;
+        for( let r = columns-1; r>= 0; r--){
+            if(!board[r][c].src.includes("blank")){
+                board[ind][c].src = board[r][c].src;
+                ind -= 1;
+            }
+        }
+        for(let r = ind; r >= 0; r--){
+            board[r][c].src = "/Images/blank.png";
+        }
+    }
 }
